ae1bca246ee3474148f1e45810ba46347ea860d2
Ino <irisha1234517@gmail.com>
2022-11-25 13:24:32 +0400

    кое  что доделала

--- src/Execution.java
+++ src/Execution.java
@@ -9,40 +9,100 @@ public class Execution {
 
     public Execution(){
         int kolProc = rnd.nextInt(3, 5);
-        for(int i = 0; i < rnd.nextInt(3, 5); i++){
-            firstQueue.addLast(new Process(i + 1)); // заполнение первой очереди
-            secondQueue.addLast(new Process(i + 1)); // заполнение второй очереди
+        System.out.println("Процессы: ");
+        for(int i = 0; i < rnd.nextInt(2, 4); i++){
+            Process first = new Process(i + 1);
+            Process second = new Process(i + 1);
+            second.Time = first.Time;
+            second.TimeLeft = first.Time;
+            System.out.println(first.Name + "  time:  " + first.Time);
+            firstQueue.addLast(first); // заполнение первой очереди
+            secondQueue.addLast(second); // заполнение второй очереди
         }
     }
     public void Interruption() // с прерыванием
     {
-        Process saved;
-        while(!firstQueue.isEmpty()){
+        Process saved = null;
+        int TimeIO;
+        int IO = 1;
+        int kol;
+        boolean ifIO;
+
+
+        while(!firstQueue.isEmpty())
+        {
+            kol = 0;
             Process process = firstQueue.getFirst();
             firstQueue.removeFirst();
             System.out.println("Работает: " + process.Name);
             System.out.print("   ");
 
-            boolean ifIO;
+            ifIO = rnd.nextBoolean(); //будет ли работа с IO
+            if(ifIO){
+                IO = rnd.nextInt(process.Time); //на каком моменте будет работа с IO
+            }
+
             for(int i = 0; i < process.Time; i++){
-                if(i == )
-                ifIO = rnd.nextBoolean();
-                if(ifIO){
+                if(kol == Time){
+
+                    break;
+                }
+
+                if(i == IO){
+                    TimeIO = rnd.nextInt(1, 3); //время работы IO
                     System.out.print("IO->блокировка");
-                    saved = process;
+                    saved = process; //сохранение процесса
                     break;
                 }
                 else{
                     System.out.print("...");
                     process.TimeLeft--;
+
                 }
             }
+
+            if(saved == null){
+
+            }
+
         }
     }
 
     public void NonInterruption() // без прерывания
     {
+        System.out.println("Без прерываний");
+        int kol;
+        while(!secondQueue.isEmpty()){
+            kol = 0;
+            Process process = secondQueue.getFirst();
+            secondQueue.removeFirst();
+            System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");
+            System.out.print("Работает: " + process.Name + " [" + process.TimeLeft + "]  =>");
 
+            boolean ifIO;
+            for(int i = 0; i < process.TimeLeft; i--){
+                if(kol == Time){
+                    if(process.TimeLeft > 0)
+                        System.out.println(" => приостановка процесса");
+                    break;
+                }
+
+                ifIO = rnd.nextBoolean();
+                if(ifIO){
+                    System.out.print(" IO ");
+                    kol++;
+                }
+                else{
+                    System.out.print(" ... ");
+                    process.TimeLeft--;
+                    kol++;
+                }
+            }
+            if(process.TimeLeft > 0) secondQueue.addLast(process);
+            else  System.out.println(" => завершен");
+            System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");
+            System.out.println();
+        }
     }
 
 }
