c8ffa2a76cc3e9bc413a2f57d7dcfc43b4f575dd
Inohara <irisha1234517@gmail.com>
2022-12-04 19:15:42 +0400

    изменения

--- src/Execution.java
+++ src/Execution.java
@@ -2,55 +2,61 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class Execution {
-    private Thread first;
-    private Thread second;
+    private LinkedList<Thread> queue;
+    private LinkedList<Thread> saved;
     private Random rnd = new Random();
     private int Time = 6; //максимальное время для одного процесса
 
     public Execution(){
-        first = new Thread(1);
-        second = new Thread(2);
+        queue = new LinkedList<>();
+        queue.addLast(new Thread(1, true));
+        queue.addLast(new Thread(2, false));
         System.out.println("Процессы: ");
-        System.out.println(first.Name + "  time:  " + first.Time);
-        System.out.println(second.Name + "  time:  " + second.Time);
+        for(Thread item : queue){
+            System.out.println(item.Name + "  time:  " + item.Time);
+        }
     }
+
     public void Interruption() // с прерыванием
     {
         int IOTime = rnd.nextInt(2,7);
-        int start = rnd.nextInt(2, second.Time - 2);
         System.out.println(IOTime);
-        System.out.println(start);
+        Thread current;
+        boolean flag = false;
 
-        System.out.print(first.Name + "[" + first.TimeLeft + "] : ");
+        while(!queue.isEmpty()){
+            current = queue.getFirst();
+            queue.removeFirst();
 
-        while(first.TimeLeft != 0){
-            if(first.Time - first.TimeLeft + 1 == start){
-                System.out.print("IO ");
+            System.out.print(current.Name + "[" + current.TimeLeft + "] : ");
+            for(int i = 0; i < Time; i++){
+                if(i == saved.getFirst().IOTime){
 
-                System.out.println();
-                System.out.print(second.Name + "[" + second.TimeLeft + "] : ");
-                while(second.TimeLeft != 0){
-                    if(second.Time - second.TimeLeft == IOTime){
-                        break;
-                    }
-                    System.out.print("... ");
-                    second.TimeLeft--;
                 }
-                System.out.println();
-                System.out.print(first.Name + "[" + first.TimeLeft + "] : ");
+                if(current.TimeLeft == 0){
+                    break;
+                }
+                if(current.IO && current.Time - current.TimeLeft == current.IOStart){
+                    System.out.print("IO ");
+                    flag = true;
+                    current.IO = false;
+                    break;
+                }
+
+                System.out.print("... ");
+                current.TimeLeft--;
             }
-            System.out.print("... ");
-            first.TimeLeft--;
+            if(current.TimeLeft == 0){
+                System.out.print("завершен");
+            }
+            else if(current.Time - current.TimeLeft == current.IOStart){
+                saved.addLast(current);
+            }
+            else{
+                queue.addLast(current);
+                System.out.print("завершен");
+            }
         }
-
-        System.out.println();
-        System.out.print(second.Name + "[" + second.TimeLeft + "] : ");
-        while (second.TimeLeft != 0){
-            System.out.print("... ");
-            second.TimeLeft--;
-        }
-
-        System.out.println("Общее время выполнения: ");
     }
 
     public void NonInterruption() // без прерывания
--- src/Thread.java
+++ src/Thread.java
@@ -4,11 +4,19 @@ public class Thread {
     public String Name;
     public int Time;
     public int TimeLeft;
+    public int IOStart;
+    public int IOTime;
+    public boolean IO;
 
-    public Thread(int num){
+    public Thread(int num, boolean io){
         Name = "Поток№" + num;
         Random rnd = new Random();
         Time = rnd.nextInt(8, 14);
         TimeLeft = Time;
+        IO = io;
+        if(IO){
+            IOStart = rnd.nextInt(4, Time - 4);
+            IOTime = rnd.nextInt(4, Time - 4);
+        }
     }
 }
