34725025235c0633b57b0f2cd8f5afd82d2b5994
Hells Hound <irisha1234517@gmail.com>
2022-12-06 09:23:55 +0400

    Ввод вывод, управляемый прерываниями

--- src/Execution.java
+++ src/Execution.java
@@ -2,47 +2,83 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class Execution {
-    private LinkedList<Thread> queue;
-    private LinkedList<Thread> saved;
+    private LinkedList<Thread> queueF;
+    private LinkedList<Thread> queueS;
     private Random rnd = new Random();
     private int Time = 7; //максимальное время для одного процесса
 
     public Execution(){
-        queue = new LinkedList<>();
-        queue.addLast(new Thread(1,  18, 4, 8));
-        queue.addLast(new Thread(2,  16, 0, 0));
+        queueF = new LinkedList<>();
+        queueF.addLast(new Thread(1,  18, 4, 8));
+        queueF.addLast(new Thread(2,  16, -1, -1));
+
+        queueS = new LinkedList<>();
+        queueS.addLast(new Thread(1,  18, 4, 8));
+        queueS.addLast(new Thread(2,  16, -1, -1));
+
         System.out.println("Процессы: ");
-        for(Thread item : queue){
-            System.out.println(item.Name + "  time:  " + item.Time);
+        for(Thread item : queueF){
+            System.out.print(item.Name + "  time:  " + item.Time);
+            if(item.IOTime != -1) System.out.print("  IOTime:  " + item.IOTime);
+            System.out.println();
         }
     }
 
-    public void Interruption() // с прерыванием
+    public void Interruption()
     {
+        System.out.println("=============================================");
+        System.out.println("Ввод вывод, управляемый прерываниями");
         Thread current;
-        boolean flag = false;
+        Thread saved;
+        int allTime = 0;
         int res;
         //1 - завершен
         //0 - IO, прерывание
         //-1 - не завершен
 
-        while(!queue.isEmpty()){
-            current = queue.getFirst();
-            queue.removeFirst();
-            res = current.Interruption(Time);
+        while(!queueF.isEmpty()){
+            current = queueF.getFirst();
+            queueF.removeFirst();
+            res = current.Do(Time);
+            allTime += Time;
             if(res == 0){
+                saved = current;
+                int need = saved.IOTime;
+                current = queueF.getFirst();
+                queueF.removeFirst();
 
+                int kol = need / Time + 1;
+                while(kol > 0){
+                    if(need <= Time){
+                        current.Do(need);
+                        allTime += need;
+                    }
+                    else{
+                        current.Do(Time);
+                        allTime += Time;
+                    }
+                    kol--;
+                    need -= Time;
+                }
+
+                if(current.WorkTime < current.Time) queueF.addLast(current);
+
+                if(saved.Ost != 0){
+                    saved.Do(saved.Ost);
+                    allTime += saved.Ost;
+                    saved.Ost = 0;
+                    if(saved.WorkTime < saved.Time) queueF.addLast(saved);
+                }
             }
 
             if(res == -1) //идет на еще один круг
-                queue.addLast(current);
-
+                queueF.addLast(current);
         }
+        System.out.println("Все время работы: " + allTime);
     }
 
     public void NonInterruption() // без прерывания
     {
 
     }
-
 }
--- src/Thread.java
+++ src/Thread.java
@@ -1,12 +1,12 @@
-import java.util.Random;
-
 public class Thread {
     public String Name;
     public int Time;
     public int WorkTime;
+    public int Ost;
     public int IOStart;
     public int IOStop;
     public int IOTime;
+    public boolean IOEnd = true;
 
     public Thread(int num, int Time, int IOStart, int IOTime){
         Name = "Поток№" + num;
@@ -15,27 +15,33 @@ public class Thread {
         this.IOStart = IOStart;
         this.IOTime = IOTime;
         IOStop = IOStart + IOTime;
+        if(IOTime != -1) IOEnd = false;
     }
 
-    public int Interruption(int timer){
+    public int Do(int timer){
         //1 - завершен
         //0 - IO, прерывание
         //-1 - не завершен
 
-        System.out.print(Name + "[" + WorkTime + "] : ");
+        System.out.print(Name + "[" + (Time - WorkTime) + "] : ");
 
         for(int i = 1; i <= timer; i++){
-            if(WorkTime == timer){
+            if(WorkTime == Time){
                 System.out.print("завершен");
+                System.out.println();
                 return 1;
             }
-            if(WorkTime == IOStart){
-                System.out.print("IO => прерывание");
+            if(WorkTime == IOStart & !IOEnd){
+                System.out.print("IO");
+                System.out.println();
+                Ost = timer - WorkTime;
+                IOEnd = true;
                 return 0;
             }
             System.out.print("... ");
             WorkTime++;
         }
+        System.out.println();
         return -1;
     }
 }
